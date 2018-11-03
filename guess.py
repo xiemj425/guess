451873fdc8432cb41e8b9ced8dfc4cb5595594d6
Ming-Jie Hsieh <mingjie0425@gmail.com>
2018-11-03 23:36:35 +0800

    Try exception

--- guess.py
+++ guess.py
@@ -1,16 +1,18 @@
 import random
 r = random.randint(1, 100)
-r1 = int( input('請猜數字1~100:'))
-
-while (r1 < 1 or r1 > 100):
-	r1 = int( input('請猜數字1~100:'))
-
-while r1 != r:
-	if r1 < r:
-		print('比答案小')
+r1 = input('請猜數字 1 ~ 100: ')
+while True:
+	if str.isnumeric(r1) == False:
+		r1 = input('輸入錯誤！ 請猜 “數字” 1 ~ 100: ')
 	else:
-		print ('比答案大')
-	r1 = int(input('請猜數字1~100:'))
+		if (int(r1) < 1 or int(r1) > 100):
+			r1 = input('輸入錯誤！ 請猜 數字 ”1 ~ 100": ')
+		elif int(r1) != r:
+			if int(r1) < r:
+				r1 = input('比答案小, 再猜猜看:')
+			else:
+				r1 = input('比答案大, 再猜猜看:')
+		else: 
+			print('終於猜對了！')
+			break
 
-if r1==r:
-	print('終於猜對了')
